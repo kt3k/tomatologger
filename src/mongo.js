eef8b72dd2cdea1a63eece57346f0f2bf5549bbf
Yoshiya Hinosawa <stibium121@gmail.com>
2015-03-15 19:03:51 +0900

    Add mongo.js

---
+++ src/mongo.js
@@ -0,0 +1,44 @@
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
+
+mongoose.Model.prototype.psave = function () {
+
+    var that = this;
+
+    return new Promise(function (resolve) {
+
+        that.save(function (err) {
+
+            if (err) {
+                throw err
+            }
+
+            resolve();
+
+        });
+
+    });
+
+};
+
+
+var Tomato = mongoose.model('Tomato', {
+    title: String,
+    notes: String,
+    mood: String,
+    performance: Number,
+    startedAt: Date,
+    endedAt: Date,
+    interupted: Boolean,
+    finished: Boolean,
+    accountId: String
+});
+
+var Account = mongoose.model('Account', {
+    name: String
+});
+
+
+module.exports.Tomato = Tomato;
+module.exports.Account = Account;
