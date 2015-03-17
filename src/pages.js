e37a7adb0d2ce8aa09b17060da2f98e7242c4ab4
Yoshiya Hinosawa <stibium121@gmail.com>
2015-03-17 16:15:33 +0900

    Tweak routing

--- src/pages.js
+++ src/pages.js
@@ -5,10 +5,14 @@ var auth = require('./auth');
 
 module.exports.addRoutes = function (app) {
 
-    app.use('/login.html', auth, auth.loginRedirect('/main'));
+    app.use('/login.html', auth, auth.loginRedirect('/main.html'));
 
+    app.use('/main.html', auth, auth.noLoginRedirect('/login.html'));
 
-    app.use('/main.html', auth, auth.noLoginRedirect('/'), function (req, res) {
+    app.get('/', function (req, res) {
+
+        res.redirect('/login.html');
+
     });
 
 };
