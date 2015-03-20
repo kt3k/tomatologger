017dda8d23662f5c0dbcbddde6a39a90d9132d1d
Yoshiya Hinosawa <stibium121@gmail.com>
2015-03-20 23:31:09 +0900

    Add ajax-view

---
+++ public/bower_components/ajax-view/ajax-view.js
@@ -0,0 +1,80 @@
+/**
+ * ajax-view.js v0.1.4
+ * author: Yoshiya Hinosawa ( http://github.com/kt3k )
+ * license: MIT
+ */
+
+(function ($, qwest, dust) {
+    'use strict';
+
+    var getDataParams = function ($dom) {
+
+        var result = {};
+
+        $.each($dom[0].attributes, function (attr) {
+
+            result[attr.name] = attr.value;
+
+        });
+
+        return result;
+    };
+
+    var initAjaxView = function () {
+
+        $('.ajax-view:not(.ajax-view-started)').each(function () {
+
+            var $dom = $(this);
+
+            $dom.addClass('ajax-view-started');
+
+            var api = $dom.attr('data-api');
+            var method = $dom.attr('data-method') || 'get';
+            var params = getDataParams($dom);
+            var template = $dom.attr('data-template');
+            var responseType = $dom.attr('data-response-type') || 'json';
+
+
+            if (!api) {
+
+                throw new Error('api is not specified: ' + api);
+
+            }
+
+            if (!/^(get|post|put|delete)$/.test(method)) {
+
+                throw new Error('unknown method: ' + method);
+
+            }
+
+            // makes a request
+            qwest[method](api, params, {responseType: responseType}).then(function (resp) {
+
+                var templateString = $(template).text().replace(/^\s*|\s*$/g, '');
+
+                dust.renderSource(templateString, resp, function (err, html) {
+
+                    $dom.html(html);
+
+                });
+
+            }).catch(function (e) {
+
+                console.log(e);
+
+            });
+
+        });
+
+    };
+
+
+    $(document).on('init.ajax-view', initAjaxView);
+
+    $(document).ready(function () {
+
+        $(document).trigger('init.ajax-view');
+
+    });
+
+}(window.jQuery, window.qwest, window.dust));
