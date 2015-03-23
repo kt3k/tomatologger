bed2e0f78117ccc736c94ba8743b77a9d5708abd
Yoshiya Hinosawa <stibium121@gmail.com>
2015-03-23 17:09:13 +0900

    Add api_host variable

--- config.rb
+++ config.rb
@@ -5,6 +5,8 @@ set :js_dir, 'js'
 
 set :images_dir, 'img'
 
+set :api_host, 'http://localhost:18000'
+
 ready do
 
   sprockets.append_path File.join root, 'bower_components'
@@ -16,5 +18,7 @@ configure :build do
   activate :minify_css
 
   activate :minify_javascript
+
+  set :api_host, ''
 
 end
