29616e5bc6b441bd90d576db00c794e6c8da3a89
Yoshiya Hinosawa <stibium121@gmail.com>
2015-03-23 16:56:46 +0900

    Allow cors in dev

--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,10 @@ var api = require('./api');
 module.exports = function (app) {
     'use strict';
 
+    if (process.env.APP_STATE !== 'production') {
+        app.use(auth.corsOK());
+    };
+
     auth.addRoutes(app);
 
     pages.addRoutes(app);
