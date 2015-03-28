489365e6216d1aee3248b477138e8edb3d599836
Yoshiya Hinosawa <stibium121@gmail.com>
2015-03-28 21:58:45 +0900

    Rename main to home

--- src/pages.js
+++ src/pages.js
@@ -5,9 +5,9 @@ var auth = require('./auth');
 
 module.exports.addRoutes = function (app) {
 
-    app.use('/login.html', auth, auth.loginRedirect('/main.html'));
+    app.use('/login.html', auth, auth.loginRedirect('/home.html'));
 
-    app.use('/main.html', auth, auth.noLoginRedirect('/login.html'));
+    app.use('/home.html', auth, auth.noLoginRedirect('/login.html'));
 
     app.get('/', function (req, res) {
 
